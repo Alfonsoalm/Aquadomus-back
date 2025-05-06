315ca7e7fb7445b9b6cf619f64e5e467db28a515
CTM40 <CTM40@cetemet.local>
2025-05-06 14:46:46 +0200

    Cambio en start.sh 5

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sleep 5
-apt-get update && apt-get install -y tailscale
+apt-get update 
+apt-get install -y tailscale
 tailscale up --accept-routes --auth-key=$TAILSCALE_AUTHKEY &
 npm start
