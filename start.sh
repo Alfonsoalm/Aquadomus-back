994726064016764677cbc9d4edb28dda8bf1a582
CTM40 <CTM40@cetemet.local>
2025-05-06 14:58:57 +0200

    Cambio en start.sh 8

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -fsSL https://tailscale.com/install.sh | sh
-sudo systemctl start tailscaled.service
-sudo tailscale up --auth-key=tskey-auth-kSDhvmp9nS11CNTRL-qcyBw1ex5n79TMUhv31Jn7mD7GkhUBnd --advertise-exit-node &
+sudo /usr/sbin/tailscaled --statedir /var/lib/tailscale --socket /var/run/tailscale/tailscaled.sock &
+sudo tailscale up --accept-routes --auth-key=tskey-auth-kSDhvmp9nS11CNTRL-qcyBw1ex5n79TMUhv31Jn7mD7GkhUBnd --advertise-exit-node &
 npm start
